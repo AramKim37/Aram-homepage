728df15cd8baba30cf9f84b8a2b07f294aec7cca
AramKim37 <aramkim37@gmail.com>
2024-01-06 14:51:23 -0700

    newly build experience section

--- components/WorkExperience.jsx
+++ components/WorkExperience.jsx
@@ -4,20 +4,17 @@ import ExperienceCard from "./ExperienceCard";
 
 const WorkExperience = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 2.5 }}
-      className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
-    >
+    <div className="h-screen relative flex overflow-hidden flex-col text-left max-w-full px-10 justify-evenly mx-auto items-center md:flex-row">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
-
-      <div className="w-full space-x-5 flex-col">
+      <div>
+        <ExperienceCard />
+        <ExperienceCard />
+        <ExperienceCard />
         <ExperienceCard />
       </div>
-    </motion.div>
+    </div>
   );
 };
 
