bbca5bff44ae333821ff267933b91ab9a529c27c
AramKim37 <aramkim37@gmail.com>
2024-01-10 23:23:39 -0700

    add send email icon in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,8 +4,6 @@ import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import Image from "next/image";
-import Email from "@/public/image/email.png";
 
 const Header = () => {
   return (
@@ -74,7 +72,21 @@ const Header = () => {
             bgColor="transparent"
           /> */}
           <div className="flex space-x-3 items-center">
-            <Image src={Email} alt="email" className="h-8 w-8 text-gray-500" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 fill-gray-400"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+              />
+            </svg>
+
             <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
               Get In Touch
             </p>
