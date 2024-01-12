bf55051da137a002d29228afaf9d706bcc1c202a
AramKim37 <aramkim37@gmail.com>
2024-01-11 17:05:18 -0700

    text color changed in about section

--- components/About.jsx
+++ components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
       transition={{ duration: 3.5 }}
       className="flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center h-screen"
     >
-      <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl md:top-24">
+      <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl md:top-2">
         About
       </h3>
 
@@ -19,27 +19,29 @@ const About = () => {
           x: -200,
           opacity: 0,
         }}
-        transition={{ duration: 1.7 }}
+        transition={{ duration: 1.5 }}
         whileInView={{
           x: 0,
           opacity: 1,
         }}
         viewport={{ once: true }}
         src={Aram.src}
-        className="-mb-20 md:mb-0 flex-shrink-0 w-40 h-40 rounded-full object-cover md:rounded-lg md:w-[400px] md:h-[500px] xl:w-[600px] xl:h-[600px]"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-40 h-40 rounded-full object-cover md:rounded-lg md:w-[300px] md:h-[400px] xl:w-[500px] xl:h-[400px]"
       />
-      <div className="space-y-2 px-0 md:px-10">
-        <h4 className="text-xl font-semibold">Who I am</h4>
-        <p className="text-[12px] md:text-base">
-          As a recent graduate of the Southern Alberta Institute of Technology
-          in the information Technology - Software Development Program. Since I
-          started studying computer science, I am always passionate and
-          motivated about coding. Also, I am very persistent and a lifelong
-          learner. So I am willing to learn new tech skills. In addtion, I am a
-          qualified developer with strong creative and analytical skills. Team
-          player with an eye for detail, extremely motivated to constantly
-          develop my skills and grow professionally. I am confident that I will
-          work well with a team.
+      <div className="space-y-4 px-0 md:px-10">
+        <h4 className="text-xl font-semibold md:text-3xl text-gray-500">
+          Who I am
+        </h4>
+        <p className="text-[12px] text-gray-500 md:text-base">
+          Graduate of the Southern Alberta Institute of Technology in the
+          information Technology - Software Development Program. Since I started
+          studying computer science, I am always passionate and motivated about
+          coding. Also, I am very persistent and a lifelong learner. So I am
+          willing to learn new tech skills. In addtion, I am a qualified
+          developer with strong creative and analytical skills. Team player with
+          an eye for detail, extremely motivated to constantly develop my skills
+          and grow professionally. I am confident that I will work well with a
+          team.
         </p>
       </div>
     </motion.div>
