6277de8ce8b3f789f148261a97aba6b9c21e8284
AramKim37 <aramkim37@gmail.com>
2024-01-20 18:05:05 -0700

    modify grid layout to relative position with hover effect

--- components/WorkExperience.jsx
+++ components/WorkExperience.jsx
@@ -8,37 +8,49 @@ const WorkExperience = () => {
   return (
     <div
       id="experience"
-      className="flex flex-col space-y-4 items-center justify-center text-center p-10 "
+      className="w-full flex flex-col space-y-4 items-center justify-center text-center p-10 "
     >
       <h1 className="pb-20 uppercase text-center tracking-[20px]  text-gray-500 text-2xl 2xl:text-4xl">
         experience
       </h1>
-      <div>
-        <div>
-          {experienceCard.map((item) => (
-            <div key={item.id}>
-              <div className="">
-                <Image
-                  src={item.image}
-                  alt="capstone"
-                  width={70}
-                  height={70}
-                  objectFit="cover"
-                  className="w-[300px] h-[300px] object-center object-cover"
-                />
-              </div>
-              <h1>{item.title}</h1>
-              <div>
-                {Object.entries(item.icons[0]).map(([key, value]) => (
-                  <div key={key}>
-                    <Image src={value} alt="icons" width={32} height={32} />
+      <div className="w-full h-auto">
+        <div className="flex flex-col md:flex-row items-center justify-center space-x-10">
+          {experienceCard.map((item, index) => (
+            <div key={item.id} className="">
+              <div className="w-full relative">
+                <div className="flex items-center justify-center w-[400px] h-[400px]">
+                  <Image
+                    src={item.image}
+                    alt="capstone"
+                    width={320}
+                    height={320}
+                    objectFit="cover"
+                    className="w-full h-full object-center object-cover rounded-xl "
+                  />
+                </div>
+                <div className="absolute w-full h-full bottom-0 flex flex-col items-center justify-evenly  bg-gray-500">
+                  <h1>{item.title}</h1>
+                  <div className="flex space-x-5">
+                    {Object.entries(item.icons[0]).map(([key, value]) => (
+                      <div key={key} className="">
+                        <Image
+                          src={value}
+                          alt="icons"
+                          width={32}
+                          height={32}
+                          className=""
+                        />
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>
-              <div>
-                {Object.entries(item.desc[0]).map(([key, value]) => (
-                  <li key={key}>{value}</li>
-                ))}
+                  <div>
+                    {Object.entries(item.desc[0]).map(([key, value]) => (
+                      <li className="text-left" key={key}>
+                        {value}
+                      </li>
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
           ))}
