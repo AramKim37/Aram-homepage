ae6707fc24095c45c5aa4267083e5d7436ff4217
AramKim37 <aramkim37@gmail.com>
2024-01-27 10:24:52 -0700

    need to change icon

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -9,17 +9,30 @@ import Image from "next/image";
 
 const SideBar = () => {
   return (
-    <div className="">
-      <div className="w-fit flex md:flex-col items-center justify-center gap-2 bg-slate-400 p-2">
-        <div>
-          <Image src={Home} alt="" width={40} height={40} />
-          <span className="">HOME</span>
-        </div>
-        <Image src={Experience} alt="" width={40} height={40} />
-        <Image src={Projects} alt="" width={40} height={40} />
-        <Image src={Skills} alt="" width={40} height={40} />
+    <div className="w-fit flex md:flex-col items-center justify-center gap-2 bg-slate-400 p-2">
+      <div className="">
+        <Image src={Home} alt="" width={40} height={40} />
+        <span className="">HOME</span>
+      </div>
+      <div>
         <Image src={About} alt="" width={40} height={40} />
+        <span className="">About</span>
+      </div>{" "}
+      <div>
+        <Image src={Experience} alt="" width={40} height={40} />
+        <span className="">Experience</span>
+      </div>{" "}
+      <div>
+        <Image src={Projects} alt="" width={40} height={40} />
+        <span className="">Projects</span>
+      </div>{" "}
+      <div>
+        <Image src={Skills} alt="" width={40} height={40} />
+        <span className="">Skills</span>
+      </div>{" "}
+      <div>
         <Image src={Contact} alt="" width={40} height={40} />
+        <span className="">Contact</span>
       </div>
     </div>
   );
