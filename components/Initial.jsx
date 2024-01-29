b42da2a28a6ef52a83e5bf3fa20bc644ff208cb6
AramKim37 <aramkim37@gmail.com>
2024-01-28 22:51:06 -0700

    add motion framer in initial

--- components/Initial.jsx
+++ components/Initial.jsx
@@ -1,20 +1,44 @@
 import Link from "next/link";
 import React from "react";
 import Hero from "./Hero";
+import { motion } from "framer-motion";
 
 const Initial = () => {
   return (
     <div className="w-max-7xl min-h-screen flex flex-col justify-center text-white px-20 ">
-      <h1 className="text-7xl md:text-8xl font-extrabold flex flex-col justify-start uppercase">
-        Welcome to my <span className="headerText">world!</span>
-      </h1>
-      <div className="w-full text-right leading-10 py-20">
+      <motion.div
+        initial={{ opacity: 0, x: -300 }}
+        transition={{ duration: 1.5 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        className="text-7xl md:text-8xl font-extrabold flex flex-col justify-start uppercase"
+      >
+        Welcome to my
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: 300 }}
+        transition={{ duration: 1.5 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        className="headerText text-7xl md:text-8xl uppercase font-extrabold"
+      >
+        world!
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 300 }}
+        transition={{ duration: 1.5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        className="w-full text-right leading-10 py-20"
+      >
         <span className="text-3xl md:text-5xl text-right">
           I`m Aram, an Software developer who is enthusiastic about creating
           engaging and delightful Tech experiences{" "}
         </span>
-      </div>
-      <div className="light-button flex flex-col items-center p-5">
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: -300 }}
+        transition={{ duration: 1.5 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        className="light-button flex flex-col items-center p-5"
+      >
         <Link href="/hero">
           <button className="bt">
             <div className="light-holder">
@@ -26,7 +50,7 @@ const Initial = () => {
             </div>
           </button>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 };
